704cb357b993f48d831871e154e8438b7ebf2341
saulmoniq <97468464+saulmoniq@users.noreply.github.com>
2022-08-02 15:17:09 -0400

    Added the proper models in models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_lenght=20)
+    quantity = models.IntegerField(max_lenght=4)
+    price = models.IntegerField(max_lenght=4)
+
+    def __str__(self):
+        return self.name + " | " + " Cantidad disponible:" + self.quantity + " Precio: " + self.price
+
+class menuItem(models.Model):
+    name = models.CharField(max_lenght=20)
+    price = models.IntegerField(max_lenght=4)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + " | " + " Ingredientes usados: " + self.ingredient + " Precio: " + self.price
+
+class purchased (models.Model):
+    hour = models.DateTimeField()
+    menuItem = models.ForeignKey(menuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.hour + " | " + " Plato comprado: " + self.menuItem + " Ingredientes usados: " + self.ingredient

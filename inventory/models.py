0d65c25105b000ce14057480607aeff5853d1c0d
saulmoniq <97468464+saulmoniq@users.noreply.github.com>
2022-08-03 14:59:24 -0400

    Test de django

--- inventory/models.py
+++ inventory/models.py
@@ -5,14 +5,14 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_lenght=20)
     quantity = models.IntegerField(max_lenght=4)
-    price = models.IntegerField(max_lenght=4)
+    price = models.IntegerField()
 
     def __str__(self):
         return self.name + " | " + " Cantidad disponible:" + self.quantity + " Precio: " + self.price
 
 class menuItem(models.Model):
     name = models.CharField(max_lenght=20)
-    price = models.IntegerField(max_lenght=4)
+    price = models.IntegerField()
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
 
     def __str__(self):

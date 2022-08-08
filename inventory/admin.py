6dba2020b907128c5a4774ce6bb70f0cd54513b0
saulmoniq <97468464+saulmoniq@users.noreply.github.com>
2022-08-08 14:15:46 -0400

    DB now working properly

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from inventory import models
 
 # Register your models here.
+admin.site.register(models.Ingredient)
+admin.site.register(models.menuItem)
+admin.site.register(models.purchased)
+admin.site.register(models.RecipeRequirement)
+
+# @admin.register(RecipeRequirement)
+# class RecipeRequirement(admin.ModelAdmin):
+#   list_display = [field.name for field in
+# RecipeRequirement._meta.get_fields()]
--- inventory/models.py
+++ inventory/models.py
@@ -1,27 +1,34 @@
+from tkinter import CASCADE
 from django.db import models
-
+import datetime
 # Create your models here.
 
 class Ingredient(models.Model):
-    name = models.CharField(max_lenght=20)
-    quantity = models.IntegerField(max_lenght=4)
-    price = models.IntegerField()
+    name = models.CharField(max_length=20)
+    quantity = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+    unit = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
-        return self.name + " | " + " Cantidad disponible:" + self.quantity + " Precio: " + self.price
+        return self.name + " | " + " Cantidad disponible:" + str(self.quantity) + " Precio: " + str(self.price)
 
 class menuItem(models.Model):
-    name = models.CharField(max_lenght=20)
-    price = models.IntegerField()
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    price = models.FloatField(default=0)
 
     def __str__(self):
-        return self.name + " | " + " Ingredientes usados: " + self.ingredient + " Precio: " + self.price
+        return self.name + " | " + " Ingredientes usados: " + " Precio: " + str(self.price)
+
 
 class purchased (models.Model):
-    hour = models.DateTimeField()
+    hour = models.DateTimeField(default="")
     menuItem = models.ForeignKey(menuItem, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.hour + " | " + " Plato comprado: " + self.menuItem + " Ingredientes usados: " + self.ingredient
+        return str(self.hour) + " | " + " Plato comprado: " +" Ingredientes usados: "
+
+
+class RecipeRequirement(models.Model):
+    quantity = models.FloatField(default=0)
+    menuItem = models.ForeignKey(menuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient,  blank=True, on_delete=models.CASCADE)
